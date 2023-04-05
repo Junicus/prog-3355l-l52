3815de2e74be6aae5300c0dd9459832824e1dbb8
Junicus <junicus@outlook.com>
2023-04-05 16:46:42 -0400

    initial commit

---
+++ src/Lab52/Pages/Index.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Options;
+
+namespace Lab52.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly QuestionsSettings _questions;
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger, IOptions<QuestionsSettings> questions)
+    {
+        _questions = questions.Value;
+        _logger = logger;
+    }
+
+    public List<QuestionSettings>? Questions { get; set; }
+
+    public void OnGet()
+    {
+        Questions = _questions.Questions;
+    }
+
+    public void OnPost()
+    {
+        
+    }
+}
+
+public class InputAnswers
+{
+    public string Name { get; set; }
+    public List<int> Answers { get; set; }
+}
---
+++ src/Lab52/QuestionsSettings.cs
@@ -0,0 +1,27 @@
+﻿namespace Lab52;
+
+public class QuestionsSettings
+{
+    public const string SectionName = "QuestionsSettings";
+
+    public List<QuestionSettings> Questions { get; set; }
+}
+
+public class QuestionSettings
+{
+    public string Question { get; set; }
+    public List<AnswerSettings> Answers { get; set; }
+    public QuestionType Type { get; set; }
+}
+
+public class AnswerSettings
+{
+    public string Answer { get; set; }
+    public bool IsCorrect { get; set; }
+}
+
+public enum QuestionType
+{
+    Combo,
+    Radio,
+}
