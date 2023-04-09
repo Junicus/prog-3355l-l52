9ad7eeaf6df2853c49358191e6343ef3d7004674
Junicus <junicus@outlook.com>
2023-04-09 00:35:38 -0400

    added rendering of questions, added logic to score answers cleaned up files

--- src/Lab52/Pages/Index.cshtml.cs
+++ src/Lab52/Pages/Index.cshtml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Options;
 
@@ -13,23 +14,39 @@ public class IndexModel : PageModel
     {
         _questions = questions.Value;
         _logger = logger;
+
+        Questions = _questions.Questions;
     }
 
     public List<QuestionSettings>? Questions { get; set; }
 
-    public void OnGet()
-    {
-        Questions = _questions.Questions;
-    }
+    [BindProperty] public InputAnswers InputAnswers { get; set; } = new();
 
+    public ResultScores? ResultScores { get; set; }
+    
     public void OnPost()
     {
-        
+        if (ModelState.IsValid)
+        {
+            var results = InputAnswers.Answers.Select((value, i) => _questions.Questions[i].Answers[value].IsCorrect).ToList();
+
+            ResultScores = new ResultScores
+            {
+                Name = InputAnswers.Name,
+                Score = results.Count(t => t) * 5
+            };
+        }
     }
 }
 
 public class InputAnswers
 {
-    public string Name { get; set; }
-    public List<int> Answers { get; set; }
+    [Required] public string Name { get; set; } = string.Empty;
+    public List<int> Answers { get; set; } = new();
+}
+
+public class ResultScores
+{
+    public string Name { get; set; } = string.Empty;
+    public int Score { get; set; }
 }
